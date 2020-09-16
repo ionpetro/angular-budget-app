6b6bb658db0103f5d7da0f54cf8b7aeca549a10a
ionpetro <ionpetro@gmail.com>
2020-09-16 15:22:32 +0300

    init form and item list components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { AddItemFormComponent } from './add-item-form/add-item-form.component';
+import { BudgetItemListComponent } from './budget-item-list/budget-item-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainPageComponent,
-    AddItemFormComponent
+    AddItemFormComponent,
+    BudgetItemListComponent
   ],
   imports: [
     BrowserModule,
